e390258f809f01a639b2543f880c6127e0b2735e
Ashish-nice <ashishpeddi122@gmail.com>
2025-04-15 19:35:20 +0530

    added AddBUs view

--- bus_admin/urls.py
+++ bus_admin/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("login/", views.BusAdminLoginView.as_view(), name='bus_admin_login'),
     path("dashboard/", views.BusAdminDashboardView.as_view(), name='bus_admin_dashboard'),
-    path("buses/", views.BusListView.as_view(), name='bus_list'),
-    path("bookings/", views.BookingListView.as_view(), name='booking_list'),
+    path("buses/", views.BusListView.as_view(), name='admin_bus_list'),
+    path("bookings/", views.BookingListView.as_view(), name='admin_booking_list'),
 ]
--- bus_admin/views.py
+++ bus_admin/views.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import Group
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.edit import FormView
 from django.views import View
 from django.views.generic import ListView
 from django.contrib import messages
 from django.utils.decorators import method_decorator
-from bus_booking.models import Bus, Booking
+from django.http import JsonResponse
+from django.db import transaction
+from bus_booking.models import Bus, Booking, Schedule, City, Journey
+from datetime import datetime, timedelta
+from django.utils import timezone
+import json
+
 #Creating my own decorator to check if the user is authenticated and in the BusAdmin group
 def group_required(*groups):
     def decorator(view_func):
@@ -22,6 +28,7 @@ def group_required(*groups):
                 return render(request, 'bus_admin/login.html')
         return _wrapped_view
     return decorator
+
 # Create your views here.
 class BusAdminLoginView(FormView):
     template_name = 'bus_admin/login.html'
@@ -64,3 +71,41 @@ class BookingListView(ListView):
         queryset = super().get_queryset()
         queryset = queryset.filter()
         return Booking.objects.filter(bus__manager=self.request.user.profile)
+
+@method_decorator(group_required('bus_admin'), name='dispatch')
+class AddBusView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            name = data.get('name')
+            number = data.get('number')
+            capacity = data.get('capacity')
+            fare = data.get('fare')
+            description = data.get('description', '')
+            
+            if not all([name, number, capacity, fare]):
+                return JsonResponse({'success': False, 'message': 'All fields are required'}, status=400)
+            
+            new_bus = Bus.objects.create(
+                name=name,
+                number=number,
+                capacity=int(capacity),
+                fare=float(fare),
+                description=description,
+                manager=request.user.profile
+            )
+            
+            return JsonResponse({
+                'success': True, 
+                'message': 'Bus added successfully',
+                'bus': {
+                    'id': new_bus.id,
+                    'name': new_bus.name,
+                    'number': new_bus.number,
+                    'capacity': new_bus.capacity,
+                    'fare': new_bus.fare
+                }
+            })
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': str(e)}, status=500)
+
