a213f9e96fc92f1a98616c072102eb93d953f4ac
Ashish-nice <ashishpeddi122@gmail.com>
2025-05-01 23:59:29 +0530

    updated urls to include logout url

--- bus_admin/urls.py
+++ bus_admin/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.BusAdminLoginView.as_view(), name='bus_admin_login'),
+    path("logout/", views.BusAdminLogoutView.as_view(), name='bus_admin_logout'),
     path("dashboard/", views.BusAdminDashboardView.as_view(), name='bus_admin_dashboard'),
     path("buses/", views.BusListView.as_view(), name='admin_bus_list'),
     path("bookings/", views.BookingListView.as_view(), name='admin_booking_list'),
